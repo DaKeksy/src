93f4c7e2eeee1947338167b05764904c9792902a
DaKeksy <keksyolo@gmail.com>
2014-01-08 04:26:18 +0100

    Creating Project
    Creating Project

---
+++ main/java/de/dakeksy/moreutils/MoreUtils.java
@@ -0,0 +1,16 @@
+/*
+ * *
+ *  * Created by DaKeksy
+ *  * All rights reserved
+ *
+ */
+
+package de.dakeksy.moreutils;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "", name = "", version = "")
+public class MoreUtils {
+
+
+}
---
+++ main/java/de/dakeksy/moreutils/libs/Reference.java
@@ -0,0 +1,11 @@
+/*
+ * *
+ *  * Created by DaKeksy
+ *  * All rights reserved
+ *
+ */
+
+package de.dakeksy.moreutils.libs;
+
+public class Reference {
+}
