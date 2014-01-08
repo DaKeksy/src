7eff13d7a7a5f3ae7f6eae3ccd0ebec460643ad8
DaKeksy <keksyolo@gmail.com>
2014-01-08 04:49:23 +0100

    Adding etc.
    Adding etc.

--- main/java/de/dakeksy/moreutils/libs/Reference.java
+++ main/java/de/dakeksy/moreutils/libs/Reference.java
@@ -1,11 +1,16 @@
 /*
- * *
- *  * Created by DaKeksy
- *  * All rights reserved
+ *
+ *   Created by DaKeksy
+ *   All rights reserved
  *
  */
 
 package de.dakeksy.moreutils.libs;
 
 public class Reference {
+
+    public static final String MODID = "moreutils";
+    public static final String MODNAME = "More Utils";
+    public static final String VERSION = "0.1";
+
 }
